896fff4444e1f714b7f8f69cd1ee0553a1df967f
Lucas Polonio <polonio.lucas@gmail.com>
2014-07-30 00:31:50 -0300

    making pending tests dont fail

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,17 +6,20 @@ module LinkPreviewer
     describe Parser do
 
       it 'fetches a URL contents correctly' do
+        #shouldn't depend on external resources, but it's google and it doesn't make sense to mock this
         parser = Parser.new('http://www.google.com')
         expect(parser.html).to include('<!doctype html>')
         expect(parser.html).to include('<title>Google</title>')
       end
 
-      it 'follows redirects' do
-        pending
-      end
-
       it 'gets HTML elements by tag' do
         pending
+        fail
+      end
+
+      it 'follows redirects' do
+        pending
+        fail
       end
 
     end
