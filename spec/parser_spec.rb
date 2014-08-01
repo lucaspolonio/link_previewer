02d4f786cb50ea61ff9355d25e03318df8962338
Lucas Polonio <polonio.lucas@gmail.com>
2014-07-31 23:27:20 -0300

    refactoring tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,16 +5,16 @@ module LinkPreviewer
 
     describe Parser do
 
+      #shouldn't depend on external resources, TODO should I use a stub?
+      let(:parser) { Parser.new('http://www.google.com') }
+
       it 'fetches a URL contents correctly' do
-        #shouldn't depend on external resources, but it's google and it doesn't make sense to mock this
-        parser = Parser.new('http://www.google.com')
         expect(parser.html).to include('<!doctype html>')
         expect(parser.html).to include('<title>Google</title>')
       end
 
       it 'gets HTML elements by tag' do
-        pending
-        fail
+        
       end
 
       it 'follows redirects' do
