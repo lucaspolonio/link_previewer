a206a2aa990ccb297b8244adfd6a568567d08914
Lucas Polonio <polonio.lucas@gmail.com>
2014-08-01 00:13:50 -0300

    implementing basic parser methods

--- lib/link-previewer/parser.rb
+++ lib/link-previewer/parser.rb
@@ -1,13 +1,19 @@
+require 'nokogiri'
 require 'open-uri'
 
 module LinkPreviewer
   class Parser
 
-    attr_accessor :url, :html
-
     def initialize(url)
-      @url = url
-      @html = open(url).read
+      @nokogiri_html = Nokogiri::HTML(open(url))
+    end
+
+    def tag_contents(tag)
+      @nokogiri_html.css(tag).first.content
+    end
+
+    def image_src(css_selector)
+      @nokogiri_html.css(css_selector).first['src']
     end
 
   end
--- link-previewer.gemspec
+++ link-previewer.gemspec
@@ -27,5 +27,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "pry"
   spec.add_development_dependency "pry-remote"
   spec.add_development_dependency "pry-nav"
+  spec.add_development_dependency "nokogiri"
   
 end
