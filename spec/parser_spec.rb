53f0badd3522cbe5d25160e8ec59e038b420566c
Lucas Polonio <polonio.lucas@gmail.com>
2014-08-01 00:13:28 -0300

    refactoring specs again

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -8,17 +8,12 @@ module LinkPreviewer
       #shouldn't depend on external resources, TODO should I use a stub?
       let(:parser) { Parser.new('http://www.google.com') }
 
-      it 'fetches a URL contents correctly' do
-        expect(parser.html).to include('<!doctype html>')
-        expect(parser.html).to include('<title>Google</title>')
-      end
-
       it 'gets contents of a HTML tag' do
         expect(parser.tag_contents 'title').to eq('Google')
       end
 
       it 'gets image src from selector' do
-        expect(parser.image_src '.gsok_a.gsst_e img').to include('//www.gstatic.com/inputtools/images/tia.png')
+        expect(parser.image_src 'img').to include('/images/')
       end
 
       it 'follows redirects' do
